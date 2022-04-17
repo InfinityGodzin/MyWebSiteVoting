1404bfafd04233678fcb6d9c6c7b0987245f6888
Godizn <103856431+InfinityGodzin@users.noreply.github.com>
2022-04-17 00:32:07 -0300

    First Commit
    First Commit

---
+++ Lol.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SLA PORRAKKJ</title>
+        <Link rel="stylesheet" href="KKK.css">
+    </head>
+
+    <style>
+        ul{
+            list-style: none;
+            display: flex;
+            justify-content: space-around;
+        }
+    </style>
+
+    <body>
+        <section>
+            <header>
+                <center><h1 id="lolkk">QUAL É O MELHOR CHAMPION DO LOL?</h1></center>
+                <ul>
+                    <li>
+                        <img id="a" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/25cbc1eb-cf90-4050-802d-7338d02a0c55/dbi4ycm-d89e3a61-c237-466b-bcb5-9b100fa0dbe1.png"
+                        height=450px width=450px alt="Zed Image">
+                        <a id="zed" href="https://xvideos.com/video55598415/mais_um_video_cagando_e_mijando_sentado_porque_nao_tenho_nenhuma_mulher_para_gravar_porno_comigo" target="_blank">ZED</a>
+                    </li>
+                    <li>
+                        <img id="c" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9eb92786-5e60-46ac-ae2c-d33af137f691/dauzd9a-61432f8e-f717-4025-9bc7-08f0104b8030.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzllYjkyNzg2LTVlNjAtNDZhYy1hZTJjLWQzM2FmMTM3ZjY5MVwvZGF1emQ5YS02MTQzMmY4ZS1mNzE3LTQwMjUtOWJjNy0wOGYwMTA0YjgwMzAucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.MuNv4YHLMhA-TirkX_Qfj6BgY0eU98_qknfTmtRp82c" 
+                        height=450px width=450px alt="Warwick Image">
+                        <a id="ww" href="https://xvideos.com/video55598415/mais_um_video_cagando_e_mijando_sentado_porque_nao_tenho_nenhuma_mulher_para_gravar_porno_comigo" target="_blank">WARWICK</a>
+                    </li>
+                    <li>
+                        <img id="c" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/9eb92786-5e60-46ac-ae2c-d33af137f691/d7m5q10-cc7a8323-9566-4f58-930d-668928115064.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzllYjkyNzg2LTVlNjAtNDZhYy1hZTJjLWQzM2FmMTM3ZjY5MVwvZDdtNXExMC1jYzdhODMyMy05NTY2LTRmNTgtOTMwZC02Njg5MjgxMTUwNjQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.WhjS1f8_GaABUB6RiQiMW14gw8UzHNzLlQTNODCN1iA"
+                        height=450px width=450px alt="Jax Image">
+                        <a id="jax" href="https://xvideos.com/video55598415/mais_um_video_cagando_e_mijando_sentado_porque_nao_tenho_nenhuma_mulher_para_gravar_porno_comigo" target="_blank">JAX</a>
+                    </li>
+                </ul>
+            </header>
+        </section>
+    </body>
+    <footer>
+        <style>
+            ol li{
+                list-style: none;
+            }
+        </style>
+        <ol>
+            <li id="voting">
+                Voting count: 3 People
+            </li>
+        </ol>
+    </footer>
+</html>
